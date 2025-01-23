879d3ccfbae241bd9ac246b61c30c7c38a289b47
Alexander Knopov <sknopov@gmail.com>
2025-01-23 13:19:25 -0500

    Created two sub-projects - library and sample; configured Gradle - Application converted to SB

---
+++ components/sample/src/main/java/org/sample/DemoApplication.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "org.example")
+public class DemoApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}

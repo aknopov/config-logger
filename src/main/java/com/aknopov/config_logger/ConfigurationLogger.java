88be05f19833d3fced7c0f817d954d4155dc8384
Alexander Knopov <sknopov@gmail.com>
2025-01-23 12:26:49 -0500

    Added ConfigurationLogger and SB dependencies

---
+++ src/main/java/com/aknopov/config_logger/ConfigurationLogger.java
@@ -0,0 +1,100 @@
+package com.aknopov.config_logger;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+/**
+ * Component that logs injected `@Value` fields. This code -
+ * <ol>
+ * <li>Gets all bean classes in "org.example.*" packages</li>
+ * <li>Browses their constructors</li>
+ * <li>Gets from {@code Environment} values of parameters with {@code Value} annotation, or uses their default value</li>
+ * </ol>
+ */
+@Component
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class ConfigurationLogger
+{
+    private static final Logger logger = LoggerFactory.getLogger(ConfigurationLogger.class);
+    @Autowired
+    private Environment environment;
+
+    /**
+     * Logs configuration parameters in all application beans
+     *
+     * @param appPackage - a root package to scan for beans, e.g. "org.example"
+     * @param lineHeading - a heading in the log entry for the settings output, e.g. "Service configuration"
+     */
+    @PostConstruct
+    public void logSettings(String appPackage, String lineHeading) throws ClassNotFoundException
+    {
+        // https://stackoverflow.com/posts/21430849/revisions
+        ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
+        provider.addIncludeFilter(new RegexPatternTypeFilter(Pattern.compile(".*")));
+        Set<BeanDefinition> beanDefs = provider.findCandidateComponents(appPackage);
+
+        Properties allSettings = new Properties();
+
+        for (var beanDef : beanDefs)
+        {
+            Class<?> klaz = Class.forName(beanDef.getBeanClassName());
+            allSettings.putAll(extractInjectedValues(klaz));
+        }
+
+        allSettings.entrySet()
+                .stream()
+                .map(e -> Map.entry(e.getKey().toString(), e.getValue()))
+                .sorted(Map.Entry.comparingByKey())
+                .forEach(e -> logger.info("{}: {} = {}", lineHeading, e.getKey(), e.getValue()));
+    }
+
+    private Properties extractInjectedValues(Class<?> klaz)
+    {
+        Properties ret = new Properties();
+
+        for (Constructor<?> ctor : klaz.getConstructors())
+        {
+            for (Parameter parm : ctor.getParameters())
+            {
+                Value annotation = parm.getAnnotation(Value.class);
+                if (annotation != null)
+                {
+                    Map.Entry<String, String> entry = parseAnnotation(annotation);
+                    String envValue = environment.getProperty(entry.getKey());
+                    String realValue = envValue != null ? envValue : entry.getValue();
+                    ret.setProperty(entry.getKey(), realValue != null ? realValue : entry.getValue());
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    private Map.Entry<String, String> parseAnnotation(Value annotation)
+    {
+        String unparsed = annotation.value();
+        // These are defaults
+        String[] parts = unparsed.replace("${", "")
+                .replace("}", "")
+                .split(":");
+        return Map.entry(parts[0], parts.length == 2 ? parts[1] : "N/A");
+    }
+}

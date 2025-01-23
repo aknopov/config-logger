3bec21d3902b4f1713e2dee2cd5beac5d7df83bb
Alexander Knopov <sknopov@gmail.com>
2025-01-23 12:02:45 -0500

    Initial commit

---
+++ src/main/java/com/aknopov/config_logger/Main.java
@@ -0,0 +1,14 @@
+package com.aknopov.config_logger;
+
+/**
+ * Don't skip documentation!
+ *
+ * @author sasha
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
